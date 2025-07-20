a764d2f55d86760dca870a0957dcc1f1ae1a11c2
Indroneel007 <2021ume1106@mnit.ac.in>
2025-07-20 13:31:40 +0530

    cacddc

--- db/sqlc/user_test.go
+++ db/sqlc/user_test.go
@@ -28,7 +28,7 @@ func CreateRandomUser(t *testing.T) User {
 	if err != nil {
 		t.Fatalf("failed to create user: %v", err)
 	}
-
+	//00ccac
 	require.NoError(t, err)
 	require.NotEmpty(t, user)
 	require.Equal(t, args.Username, user.Username)
